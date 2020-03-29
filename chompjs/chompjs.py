c8aa1c64b86189a20168eacedc4210b6678d6c91
Nykakin <m_obajtek@onet.eu>
2020-03-29 19:12:56 +0200

    Avoid 'During handling of the above exception...' message

--- chompjs/chompjs.py
+++ chompjs/chompjs.py
@@ -5,6 +5,7 @@ import sys
 
 from _chompjs import parse
 
+
 def parse_js_object(string, initial_stack_size=10, unicode_escape=False):
     if not string:
         raise ValueError('Invalid input')
@@ -12,12 +13,19 @@ def parse_js_object(string, initial_stack_size=10, unicode_escape=False):
         string = string.encode().decode('unicode_escape')
     if sys.version_info[0] < 3:
         string = string.encode('utf-8')
+
+    # I use this roundabout way to capture exception because Python 2.7 doesn't
+    # support `raise ... from None` syntax and I don't want to include six.rethrow
+    # only to change exception message
+    exception = None
     try:
         parsed_json = parse(string, initial_stack_size)
     except ValueError as e:
+        exception = e
+    if exception:
         if sys.version_info[0] < 3:
-            raise ValueError('Parser error: ... {}'.format(repr(str(e))[1:-1]))
+            raise ValueError('Parser error: ... {}'.format(repr(str(exception))[1:-1]))
         else:
-            raise ValueError("Parser error: ... {}".format(str(e).encode('utf-8')))
+            raise ValueError("Parser error: ... {}".format(str(exception).encode('utf-8')))
 
     return json.loads(parsed_json)
