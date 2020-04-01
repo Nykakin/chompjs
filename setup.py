62f78f53808ec69978844627ded8cc36361a3067
Nykakin <m_obajtek@onet.eu>
2020-04-01 23:19:09 +0200

    Avoiding compile errors on some platforms

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 from io import open
 from os import path
+from platform import system
 from setuptools import setup, Extension
 
 
@@ -10,17 +11,22 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+extra_compile_args = []
+extra_link_args = []
+if system() == 'Linux':
+    extra_compile_args = ['-Wl,-Bsymbolic-functions']
+    extra_link_args = ['-Wl,-Bsymbolic-functions']
 
 chompjs_extension = Extension(
     '_chompjs',
     sources=['module.c', 'parser.c'],
-    extra_compile_args=['-Wl,-Bsymbolic-functions'],
-    extra_link_args=['-Wl,-Bsymbolic-functions'],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
     name='chompjs',
-    version='1.0.7',
+    version='1.0.8',
     description='Parsing JavaScript objects into Python dictionaries',
     author='Mariusz Obajtek',
     author_email='nykakin@gmail.com',
