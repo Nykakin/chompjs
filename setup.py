6bdaad4b5629d36966384f28ae4ed52942406233
Nykakin <m_obajtek@onet.eu>
2022-08-22 12:34:49 +0200

    Fix botched version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ chompjs_extension = Extension(
 
 setup(
     name='chompjs',
-    version='1.1.7',
+    version='1.1.8',
     description='Parsing JavaScript objects into Python dictionaries',
     author='Mariusz Obajtek',
     author_email='nykakin@gmail.com',
