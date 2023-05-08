ec4689986719811f9d4ba16d28da7eff71448207
Nykakin <m_obajtek@onet.eu>
2023-05-08 05:12:59 +0200

    Add documentation

--- chompjs/chompjs.py
+++ chompjs/chompjs.py
@@ -12,49 +12,60 @@ def _preprocess(string, unicode_escape=False):
 
 
 def parse_js_object(string, unicode_escape=False, json_params=None):
-    """Extracts first JSON object it encounters in the input string
+    """
+    Extracts first JSON object encountered in the input string
 
     Parameters
     ----------
     string: str
         Input string
-        >>> parse_js_object("{a: 100}")
-        {'a': 100}
+
+    >>> parse_js_object("{a: 100}")
+    {'a': 100}
 
     unicode_escape: bool, optional
         Attempt to fix input string if it contains escaped special characters
-        >>> parse_js_object('{\\"a\\": \\"b\\"}')
-        {'\\"a\\"': '\\"b\\"'}
-        >>> parse_js_object('{\\"a\\": \\"b\\"}', unicode_escape=True)
-        {'a': 'b'}
 
-    jsonlines: bool, optional
-        Allow parsing jsonlines objects
+    json_params: dict, optional
+        Allow passing down standard json.loads flags
 
-        Raises
-        ------
-        ValueError
-            If failed to parse input properly
+    >>> parse_js_object("{'a': 10.1}")
+    {'a': 10.1}
+    >>> import decimal
+    >>> parse_js_object("{'a': 10.1}", json_params={'parse_float': decimal.Decimal})
+    {'a': Decimal('10.1')}
+
+    Returns
+    -------
+    list | dict
+        Extracted JSON object
+
+    Raises
+    ------
+    ValueError
+        If failed to parse input properly
+
+    ```python
+    >>> parse_js_object(None)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "/home/mariusz/Documents/Projekty/chompjs/chompjs/chompjs.py", line 49, in parse_js_object
+        raise ValueError('Invalid input')
+    ValueError: Invalid input
+    >>> parse_js_object("")
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "/home/mariusz/Documents/Projekty/chompjs/chompjs/chompjs.py", line 49, in parse_js_object
+        raise ValueError('Invalid input')
+    ValueError: Invalid input
+    >>> parse_js_object("{'a': 12")
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "/home/mariusz/Documents/Projekty/chompjs/chompjs/chompjs.py", line 55, in parse_js_object
+        parsed_data = parse(string)
+    ValueError: Error parsing input near character 15
+    ```
     """
-
-
-#        """Prints what the animals name is and what sound it makes.
-#
-#        If the argument `sound` isn't passed in, the default Animal
-#        sound is used.
-#
-#        Parameters
-#        ----------
-#        sound : str, optional
-#            The sound the animal makes (default is None)
-#
-#        Raises
-#        ------
-#        NotImplementedError
-#            If no sound is set for the animal or passed in as a
-#            parameter.
-#        """
-
     if not string:
         raise ValueError('Invalid input')
 
@@ -67,6 +78,46 @@ def parse_js_object(string, unicode_escape=False, json_params=None):
 
 
 def parse_js_objects(string, unicode_escape=False, omitempty=False, json_params=None):
+    """
+    Returns a generator extracting all JSON objects encountered in the input string
+
+    Parameters
+    ----------
+    string: str
+        Input string
+
+    >>> it = parse_js_objects("{a: 100} {b: 100}")
+    >>> next(it)
+    {'a': 100}
+    >>> next(it)
+    {'b': 100}
+
+    unicode_escape: bool, optional
+        Attempt to fix input string if it contains escaped special characters
+
+    omitempty: bool, optional
+        Skip empty dictionaries and lists
+
+    >>> list(parse_js_objects("{a: 12} {} {b: 13}"))
+    [{'a': 12}, {}, {'b': 13}]
+    >>> list(parse_js_objects("{a: 12} {} {b: 13}", omitempty=True))
+    [{'a': 12}, {'b': 13}]
+
+    json_params: dict, optional
+        Allow passing down standard json.loads flags
+
+    >>> next(parse_js_objects("{'a': 10.1}"))
+    {'a': 10.1}
+    >>> import decimal
+    >>> next(parse_js_objects("{'a': 10.1}", json_params={'parse_float': decimal.Decimal}))
+    {'a': Decimal('10.1')}
+
+    Returns
+    -------
+    generator
+        Iterating over it yields all encountered JSON objects
+    """
+
     if not string:
         return
 
