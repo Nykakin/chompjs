e33a4afbd70b1f9617d18152ae86f403464a74b5
Nykakin <nykakin@gmail.com>
2026-05-16 16:37:13 +0200

    Rename lexer/parser, bump the date

--- _chompjs/module.c
+++ _chompjs/module.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2025 Mariusz Obajtek. All rights reserved.
+ * Copyright 2020-2026 Mariusz Obajtek. All rights reserved.
  * License: https://github.com/Nykakin/chompjs/blob/master/LICENSE
  */
 
@@ -14,29 +14,29 @@ static PyObject* parse_python_object(PyObject *self, PyObject *args) {
         return NULL;
     }
 
-    struct Lexer lexer;
-    init_lexer(&lexer, string);
+    struct Parser parser;
+    init_parser(&parser, string);
     Py_BEGIN_ALLOW_THREADS 
-    while(lexer.lexer_status == CAN_ADVANCE) {
-        advance(&lexer);
+    while(parser.parser_status == CAN_ADVANCE) {
+        advance(&parser);
     }
     Py_END_ALLOW_THREADS
 
-    PyObject* ret = Py_BuildValue("s#", lexer.output.data, lexer.output.index-1);
-    release_lexer(&lexer);
-    if(lexer.lexer_status == ERROR) {
+    PyObject* ret = Py_BuildValue("s#", parser.output.data, parser.output.index-1);
+    release_parser(&parser);
+    if(parser.parser_status == ERROR) {
         const char* msg_sting = "Error parsing input near character %d";
         size_t error_buffer_size = snprintf(
             NULL,
             0,
             msg_sting,
-            lexer.input_position
+            parser.input_position
         );       
         char* error_buffer = malloc(error_buffer_size + 1);
         sprintf(
             error_buffer,
             msg_sting,
-            lexer.input_position - 1
+            parser.input_position - 1
         );
         PyErr_SetString(PyExc_ValueError, error_buffer);
         free(error_buffer);
@@ -47,7 +47,7 @@ static PyObject* parse_python_object(PyObject *self, PyObject *args) {
 
 typedef struct {
     PyObject_HEAD
-    struct Lexer lexer;
+    struct Parser parser;
 } JsonIterState;
 
 static PyObject* json_iter_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
@@ -60,32 +60,32 @@ static PyObject* json_iter_new(PyTypeObject *type, PyObject *args, PyObject *kwa
     if (!PyArg_ParseTuple(args, "s", &string)) {
         return NULL;
     }
-    init_lexer(&json_iter_state->lexer, string);
+    init_parser(&json_iter_state->parser, string);
 
     return (PyObject* )json_iter_state;
 }
 
 static void json_iter_dealloc(JsonIterState* json_iter_state) {
-    release_lexer(&json_iter_state->lexer);
+    release_parser(&json_iter_state->parser);
     Py_TYPE(json_iter_state)->tp_free(json_iter_state);
 }
 
 static PyObject* json_iter_next(JsonIterState* json_iter_state) {
     Py_BEGIN_ALLOW_THREADS
-    while(json_iter_state->lexer.lexer_status == CAN_ADVANCE) {
-        advance(&json_iter_state->lexer);
+    while(json_iter_state->parser.parser_status == CAN_ADVANCE) {
+        advance(&json_iter_state->parser);
     }
     Py_END_ALLOW_THREADS
 
-    if(json_iter_state->lexer.output.index == 1) {
+    if(json_iter_state->parser.output.index == 1) {
         return NULL;
     }
     PyObject* ret = Py_BuildValue(
         "s#",
-        json_iter_state->lexer.output.data,
-        json_iter_state->lexer.output.index-1
+        json_iter_state->parser.output.data,
+        json_iter_state->parser.output.index-1
     );
-    reset_lexer_output(&json_iter_state->lexer);
+    reset_parser_output(&json_iter_state->parser);
     return ret;
 }
 
--- _chompjs/parser.h
+++ _chompjs/parser.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2025 Mariusz Obajtek. All rights reserved.
+ * Copyright 2020-2026 Mariusz Obajtek. All rights reserved.
  * License: https://github.com/Nykakin/chompjs/blob/master/LICENSE
  */
 
@@ -11,7 +11,7 @@
 
 #include "buffer.h"
 
-struct Lexer;
+struct Parser;
 
 /**
     States of internal state machine:
@@ -21,11 +21,11 @@ struct Lexer;
     * end - finish work
     * error - finish work, mark an error
 */
-struct State* begin(struct Lexer* lexer);
-struct State* json(struct Lexer* lexer);
-struct State* value(struct Lexer* lexer);
-struct State* end(struct Lexer* lexer);
-struct State* error(struct Lexer* lexer);
+struct State* begin(struct Parser* parser);
+struct State* json(struct Parser* parser);
+struct State* value(struct Parser* parser);
+struct State* end(struct Parser* parser);
+struct State* error(struct Parser* parser);
 
 /*
     Helper functions used in "value" state
@@ -35,17 +35,17 @@ struct State* error(struct Lexer* lexer);
     * handle_numeric_non_standard_base - handle numbers in non-standard bases (hex, oct)
     * handle_unrecognized - save all unrecognized data as a string
 */
-struct State* handle_quoted(struct Lexer* lexer);
-struct State* handle_numeric(struct Lexer* lexer);
-struct State* handle_numeric_standard_base(struct Lexer* lexer);
-struct State* handle_numeric_non_standard_base(struct Lexer* lexer, int base);
-struct State* handle_unrecognized(struct Lexer* lexer);
+struct State* handle_quoted(struct Parser* parser);
+struct State* handle_numeric(struct Parser* parser);
+struct State* handle_numeric_standard_base(struct Parser* parser);
+struct State* handle_numeric_non_standard_base(struct Parser* parser, int base);
+struct State* handle_unrecognized(struct Parser* parser);
 
 /**
     State wrapper
 */
 struct State {
-    struct State* (*change)(struct Lexer *);
+    struct State* (*change)(struct Parser *);
 };
 
 /** Possible results of internal state machine state change state */
@@ -53,15 +53,15 @@ typedef enum {
     CAN_ADVANCE,
     FINISHED,
     ERROR,
-} LexerStatus;
+} ParserStatus;
 
 /** Main object, responsible for everything */
-struct Lexer {
+struct Parser {
     const char* input;
     size_t output_size;
     struct CharBuffer output;
     size_t input_position;
-    LexerStatus lexer_status;
+    ParserStatus parser_status;
     struct State* state;
     struct CharBuffer nesting_depth;
     size_t unrecognized_nesting_depth;
@@ -69,42 +69,42 @@ struct Lexer {
 };
 
 /** Switch state of internal state machine */
-void advance(struct Lexer* lexer);
+void advance(struct Parser* parser);
 
 /** Get next char, ignore whitespaces */
-char next_char(struct Lexer* lexer);
+char next_char(struct Parser* parser);
 
 /** Get previously handled char */
-char last_char(struct Lexer* lexer);
+char last_char(struct Parser* parser);
 
 /** Send character to output buffer, advance input position */
-void emit(char c, struct Lexer* lexer);
+void emit(char c, struct Parser* parser);
 
 /** Send character to output buffer, keep old input position */
-void emit_in_place(char c, struct Lexer* lexer);
+void emit_in_place(char c, struct Parser* parser);
 
 /** Remove last character from output buffer */
-void unemit(struct Lexer* lexer);
+void unemit(struct Parser* parser);
 
 /** Send string to output buffer, advance input position */
-void emit_string(const char *s, size_t size, struct Lexer* lexer);
+void emit_string(const char *s, size_t size, struct Parser* parser);
 
 /** Send string to output buffer, keep old input position */
-void emit_string_in_place(const char *s, size_t size, struct Lexer* lexer);
+void emit_string_in_place(const char *s, size_t size, struct Parser* parser);
 
 /** Send number to output buffer, keep old input position */
-void emit_number_in_place(long value, struct Lexer* lexer);
+void emit_number_in_place(long value, struct Parser* parser);
 
 /** Handle comments in JSON body */
-void handle_comments(struct Lexer* lexer);
+void handle_comments(struct Parser* parser);
 
-/** Initialize main lexer object */
-void init_lexer(struct Lexer* lexer, const char* string);
+/** Initialize main parser object */
+void init_parser(struct Parser* parser, const char* string);
 
-/** Reset main lexer object output buffer */
-void reset_lexer_output(struct Lexer* lexer);
+/** Reset main parser object output buffer */
+void reset_parser_output(struct Parser* parser);
 
-/** Release main lexer object and its memory */
-void release_lexer(struct Lexer* lexer);
+/** Release main parser object and its memory */
+void release_parser(struct Parser* parser);
 
 #endif
